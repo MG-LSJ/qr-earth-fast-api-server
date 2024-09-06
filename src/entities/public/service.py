229df0a8c9d0e6458695930515aa5dc8d7240939
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2024-09-06 15:26:42 +0530

    leaderboards by code count

--- src/entities/public/service.py
+++ src/entities/public/service.py
@@ -9,8 +9,8 @@ class PublicService:
     @staticmethod
     async def get_leaderboard(session: AsyncSession, limit: int) -> Sequence[User]:
         """
-        Get the leaderboard
+        Get the leaderboard based on the number of redeemed codes
         """
-        statement = select(User).order_by(desc(User.points)).limit(limit)
+        statement = select(User).order_by(desc(User.redeemed_code_count)).limit(limit)
         result = await session.exec(statement)
         return result.all()
